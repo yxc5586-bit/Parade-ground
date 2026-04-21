0e7f6506c12ada5bb638ffbd36222497fb265f39
cyx <yxc5586@gmail.com>
2026-04-21 13:48:06 +0800

    后端 - 后端初始化
    1.整合常用依赖
    2.编写各种配置

---
+++ backend/src/main/java/com/cyx/paradegroundbackend/ParadeGroundBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cyx.paradegroundbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadeGroundBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadeGroundBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cyx/paradegroundbackend/ParadeGroundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyx.paradegroundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadeGroundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
